02f987b9867260c28ec4c25240c7e6302b9cf443
fantouch <fantouchx@gmail.com>
2013-07-24 17:59:07 +0800

    添加回调方法,可以在回调方法中得到View对象和Animation对象(虽然比较少用到)

--- src/me/fantouch/libs/indicativeradio/AnimPerformer.java
+++ src/me/fantouch/libs/indicativeradio/AnimPerformer.java
@@ -6,7 +6,10 @@ import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 
 /**
- * 简化动画代码,使动画开始与动画结束相关代码块紧凑,类似线程的一般匿名类用法
+ * 动画执行器
+ * <p>
+ * 简化动画代码,使动画开始与动画结束相关代码块紧凑,类似线程的一般匿名类用法 <br>
+ * new 完会立即执行,无需调用start什么的(其实我好几次忘记调用start了,所以干脆取消start方法)
  * 
  * @author Fantouch
  */
@@ -18,35 +21,45 @@ public class AnimPerformer {
      * @param v 需要执行动画的View
      * @param anim 动画
      */
-    public AnimPerformer(final View v, Animation anim) {
+    public AnimPerformer(final View v, final Animation anim) {
         anim.setAnimationListener(new AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
                 onAnimStart();
+                onAnimStart(v, anim);
             }
 
             @Override
             public void onAnimationRepeat(Animation animation) {
                 onAnimRepeat();
+                onAnimRepeat(v, anim);
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
                 onAnimEnd();
+                onAnimEnd(v, anim);
             }
         });
 
         v.startAnimation(anim);
     }
 
-
-
     public void onAnimStart() {
+    };
+
+    public void onAnimStart(View v, Animation anim) {
     };
 
     public void onAnimRepeat() {
     };
 
+    public void onAnimRepeat(View v, Animation anim) {
+    };
+
     public void onAnimEnd() {
+    };
+
+    public void onAnimEnd(View v, Animation anim) {
     };
 }
